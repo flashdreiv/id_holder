b4c4cc81efbc8e191678fd9fb61d465cf99d57ea
Dreivan Orprecio <drei@Dreivans-MacBook-Air.local>
2025-05-22 20:51:11 +0800

    Add support for index db

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useMemo } from "react";
 import "./App.css";
 import {
   Card,
@@ -8,50 +8,46 @@ import {
 } from "./components/ui/card";
 import { Input } from "./components/ui/input";
 import { Button } from "./components/ui/button";
-import { Plus } from "lucide-react";
+import { Loader2, Plus } from "lucide-react";
+import { getAllCards, initDB, type ICard } from "./lib/db";
 
 function App() {
-  const [cards, setCards] = useState([
-    {
-      id: 1,
-      name: "Driver's License",
-      description: "00232405-34341-123123",
-      logo: "./src/assets/lto.png",
-    },
-    {
-      id: 2,
-      name: "Pagibig",
-      description: "00232405-34341-123123",
-      logo: "./src/assets/pagibig.png",
-    },
-    {
-      id: 3,
-      name: "SSS",
-      description: "123213123-123123",
-      logo: "./src/assets/sss.svg",
-    },
-    {
-      id: 4,
-      name: "TIN",
-      description: "123213123-123123",
-      logo: "./src/assets/bir.png",
-    },
-    {
-      id: 5,
-      name: "National ID",
-      description: "123213123-123123",
-      logo: "./src/assets/PSA.png",
-    },
-  ]);
+  const [isDBReady, setIsDBReady] = useState<boolean>(false);
+
+  const [cards, setCards] = useState<ICard[]>([]);
+
+  const isDataReady = useMemo(
+    () => isDBReady && cards.length > 0,
+    [isDBReady, cards]
+  );
+
+  const handleInitDB = async () => {
+    const status = await initDB();
+    setIsDBReady(status);
+  };
+
+  async function loadCards() {
+    try {
+      const cards = await getAllCards();
+      setCards(cards as ICard[]);
+    } catch (err) {
+      console.error("Failed to get cards:", err);
+    }
+  }
+
+  useEffect(() => {
+    handleInitDB();
+    loadCards();
+  }, []);
 
   const renderCards = () => {
     return cards.map((card) => (
-      <Card key={card.id} className="w-full flex py-3">
+      <Card key={card.id} className="w-full flex py-3 cursor-pointer">
         <CardHeader>
           <div className="flex justify-between items-center">
             <div className="flex flex-col gap-1">
               <CardTitle>{card.name}</CardTitle>
-              <CardDescription>{card.description}</CardDescription>
+              <CardDescription>{card.id}</CardDescription>
             </div>
             <img
               src={card.logo}
@@ -69,8 +65,13 @@ function App() {
         <h1 className="text-3xl font-bold">My Cards</h1>
         <Input placeholder="Search" onChange={() => {}} />
       </header>
+
       <main className="mt-5 flex flex-col gap-3">
-        {renderCards()}
+        {isDataReady ? (
+          renderCards()
+        ) : (
+          <Loader2 className="size-10 animate-spin text-blue-500 m-auto mt-[50%]" />
+        )}
         <footer className="fixed bottom-10 left-1/2 transform -translate-x-1/2">
           <Button className="size-12 sm:size-20 p-0 rounded-full flex items-center justify-center">
             <Plus className="size-5 sm:size-7" />
---
+++ src/lib/db.ts
@@ -0,0 +1,86 @@
+export interface ICard {
+  id: string;
+  name: string;
+  logo: string;
+}
+
+const defaultData: ICard[] = [
+  {
+    id: "1",
+    name: "Driver's License",
+    logo: "./src/assets/lto.png",
+  },
+  {
+    id: "2",
+    name: "Pagibig",
+    logo: "./src/assets/pagibig.png",
+  },
+  {
+    id: "3",
+    name: "SSS",
+    logo: "./src/assets/sss.svg",
+  },
+  {
+    id: "4",
+    name: "TIN",
+    logo: "./src/assets/bir.png",
+  },
+  {
+    id: "5",
+    name: "National ID",
+    logo: "./src/assets/PSA.png",
+  },
+];
+export const initDB = (): Promise<boolean> => {
+  return new Promise((resolve) => {
+    const request = indexedDB.open("myDB");
+
+    request.onupgradeneeded = () => {
+      const db = request.result;
+
+      if (!db.objectStoreNames.contains("cards")) {
+        db.createObjectStore("cards", { keyPath: "id" });
+      }
+    };
+
+    request.onsuccess = () => {
+      const db = request.result;
+      const tx = db.transaction("cards", "readwrite");
+      const cards = tx.objectStore("cards");
+      defaultData.forEach((card) => {
+        cards.put(card);
+      });
+
+      tx.oncomplete = () => {
+        console.log("Transaction completed");
+      };
+      tx.onerror = () => {
+        console.log("Transaction completed");
+      };
+      resolve(true);
+    };
+
+    request.onerror = () => {
+      resolve(false);
+    };
+  });
+};
+
+export async function getAllCards() {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open("myDB");
+
+    request.onsuccess = () => {
+      const db = request.result;
+      const tx = db.transaction("cards", "readonly");
+      const store = tx.objectStore("cards");
+
+      const getAll = store.getAll();
+
+      getAll.onsuccess = () => resolve(getAll.result);
+      getAll.onerror = () => reject(getAll.error);
+    };
+
+    request.onerror = () => reject(request.error);
+  });
+}
